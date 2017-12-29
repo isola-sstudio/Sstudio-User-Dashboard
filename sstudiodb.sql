f9fa0ec59eeb31accb275164b91cdbd9ca9b4910
ISOLA Tolu <isola@sstudio.io>
2017-12-29 04:03:24 +0100

    Added the db for testing

---
+++ sstudiodb.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 29, 2017 at 03:57 AM
+-- Server version: 5.7.18-1
+-- PHP Version: 7.0.20-2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `thestart_upstudio`
+--
+CREATE DATABASE IF NOT EXISTS `thestart_upstudio` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `thestart_upstudio`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tss_package_subscription`
+--
+
+CREATE TABLE `tss_package_subscription` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT 'just a normal id for the table',
+  `company_name` varchar(60) NOT NULL COMMENT 'now it''s just the name of the intended user of the admin dashboard',
+  `company_email` varchar(320) NOT NULL COMMENT 'now it''s just the email of the intended user of the admin dashboard',
+  `password` char(32) NOT NULL,
+  `contact_number` varchar(14) NOT NULL COMMENT 'contact number filled on the modal form on the pricing page',
+  `project_description` text COMMENT 'project description filled on the modal form on the pricing page',
+  `stripe_customer_id` varchar(255) DEFAULT NULL COMMENT 'stripe customer id created by at stripe''s checkout api',
+  `subscription_plan` varchar(8) DEFAULT NULL COMMENT 'the plan chosen on the pricing page, basic, standard or custom',
+  `subscription_status` char(1) NOT NULL DEFAULT '0' COMMENT 'subscription status as in active or inactive based on expired subscription or unexpired, pay now or pay later',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'time when the entry was created'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tss_package_subscription`
+--
+ALTER TABLE `tss_package_subscription`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tss_package_subscription`
+--
+ALTER TABLE `tss_package_subscription`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'just a normal id for the table';
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
