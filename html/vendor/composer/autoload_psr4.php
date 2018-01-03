6938c0cef8ea67bfbfc706d0f8a4bc70cf053929
ISOLA Tolu <isola@sstudio.io>
2018-01-03 19:54:34 +0100

    facebook graph sdk

--- html/vendor/composer/autoload_psr4.php
+++ html/vendor/composer/autoload_psr4.php
@@ -9,4 +9,5 @@ return array(
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Respect\\Validation\\' => array($vendorDir . '/respect/validation/library'),
     'Libs\\' => array($baseDir . '/Libs'),
+    'Facebook\\' => array($vendorDir . '/facebook/graph-sdk/src/Facebook'),
 );
