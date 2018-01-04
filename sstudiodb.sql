93721effa955f5d52c09fb0548548641014641ed
ISOLA Tolu <isola@sstudio.io>
2018-01-04 19:15:10 +0100

    added more columns to db

--- sstudiodb.sql
+++ sstudiodb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 04, 2018 at 07:09 AM
+-- Generation Time: Jan 04, 2018 at 07:13 PM
 -- Server version: 5.7.18-1
 -- PHP Version: 7.0.20-2
 
@@ -50,10 +50,11 @@ CREATE TABLE `tss_package_subscription` (
   `id` int(10) UNSIGNED NOT NULL COMMENT 'just a normal id for the table',
   `company_name` varchar(60) NOT NULL COMMENT 'now it''s just the name of the intended user of the admin dashboard',
   `company_email` varchar(320) NOT NULL COMMENT 'now it''s just the email of the intended user of the admin dashboard',
-  `password` char(32) NOT NULL,
-  `contact_number` varchar(14) NOT NULL COMMENT 'contact number filled on the modal form on the pricing page',
+  `password` char(32) DEFAULT NULL,
+  `contact_number` varchar(14) DEFAULT NULL COMMENT 'contact number filled on the modal form on the pricing page',
   `country` varchar(255) DEFAULT NULL,
   `picture` varchar(255) DEFAULT NULL,
+  `wallpaper` varchar(255) DEFAULT NULL,
   `project_description` text COMMENT 'project description filled on the modal form on the pricing page',
   `stripe_customer_id` varchar(255) DEFAULT NULL COMMENT 'stripe customer id created by at stripe''s checkout api',
   `subscription_plan` varchar(8) DEFAULT NULL COMMENT 'the plan chosen on the pricing page, basic, standard or custom',
